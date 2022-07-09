f50de6daa3ac52c1f821add0b7032ffc28019da6
franskbarek <franzferdinand.kbarek@gmail.com>
2022-07-09 20:42:33 +0900

    Create List and ListItem Component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,15 @@
 import Featured from "../../components/featured/Featured";
+import List from "../../components/list/List";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
 
 const Home = () => {
   return (
-    <>
-      <div className="home">
-        <Navbar />
-        <Featured />
-        <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" width="100%" />
-      </div>
-    </>
+    <div className="home">
+      <Navbar />
+      <Featured />
+      <List />
+    </div>
   );
 };
 
